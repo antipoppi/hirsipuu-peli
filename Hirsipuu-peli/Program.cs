22d8217ea05e3b99b09143393a1a75ff7f4020da
N4927 <N4927@student.jamk.fi>
2019-12-11 22:18:56 +0200

    Lisätty pelin runko. Toimii viimein!

--- Hirsipuu-peli/Program.cs
+++ Hirsipuu-peli/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace Hirsipuu_peli
 {
@@ -9,15 +7,84 @@ namespace Hirsipuu_peli
     {
         static void Main(string[] args)
         {
-            string arvattavaSana = Utils.ArvotaanSana();
-            StringBuilder näytetäänPelaajalle = new StringBuilder(arvattavaSana.Length);
-            for (int i = 0; i < arvattavaSana.Length; i++)
-            {
-                näytetäänPelaajalle.Append("-");
-            }
+            string arvattavaSana = Utils.ArvoSana();
+            string näytetäänPelaajalle = Utils.MuutaSana(arvattavaSana);
+            bool voitto = false;
+            int arvauskerrat = 10;
+            List<char> oikeatVastaukset = new List<char>();
+            List<char> väärätVastaukset = new List<char>();
 
-            Console.WriteLine(näytetäänPelaajalle);
-            Console.WriteLine(arvattavaSana);
+            Console.WriteLine("Tervetuloa pelaamaan Hirsipuu-peliä!");
+            Console.WriteLine("Voit arvata kirjaimen aakkosista A-Ö tai vastata arvattavan sanan.");
+
+            while (!voitto && arvauskerrat > 0)
+            {
+                Console.WriteLine("Sinulla on mahdollisuus arvata väärin " + arvauskerrat + " kertaa.");
+                Console.Write("\nArvattava sana on: " + näytetäänPelaajalle + " (sana on " + arvattavaSana.Length + " kirjainta pitkä) \nArvaa: ");
+                string syöte = Console.ReadLine().ToUpper();
+                if (syöte.Length == arvattavaSana.Length)
+                {
+                    if (syöte == arvattavaSana)
+                    {
+                        voitto = true;
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Vastaus on väärä!");
+                        arvauskerrat--;
+                        continue;
+                    }
+                }
+                else
+                {
+                    char arvattuKirjain = syöte[0];
+                    if (arvattavaSana.Contains(arvattuKirjain))
+                    {
+                        Console.Clear();
+                        oikeatVastaukset.Add(arvattuKirjain);
+                        for (int i = 0; i < arvattavaSana.Length; i++)
+                        {
+                            if (arvattavaSana[i] == arvattuKirjain)
+                            {
+                                näytetäänPelaajalle = Utils.PaljastaOsaSana(oikeatVastaukset, arvattavaSana);
+                            }
+                        }
+                        Console.WriteLine("Sanasta löytyy kirjain " + arvattuKirjain + ".");
+                        Console.WriteLine("Olet yrittänyt kirjaimia " + Utils.tulostaVäärät(väärätVastaukset));
+                        if (näytetäänPelaajalle == arvattavaSana)
+                        {
+                            voitto = true;
+                        }
+                    }
+                    else if (väärätVastaukset.Contains(syöte[0]))
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Olet jo yrittänyt kirjainta " + syöte[0] + ". Yritä jotain toista. (tästä ei lähtenyt yrityskertaa)");
+                        Console.WriteLine("Olet yrittänyt seuraavia vääriä kirjaimia: " + Utils.tulostaVäärät(väärätVastaukset));
+                        continue;
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        väärätVastaukset.Add(arvattuKirjain);
+                        Console.WriteLine("Sanasta ei löydy kirjainta: " + arvattuKirjain);
+                        Console.WriteLine("Olet yrittänyt seuraavia vääriä kirjaimia: " + Utils.tulostaVäärät(väärätVastaukset));
+                        arvauskerrat--;
+                        continue;
+                    }
+                }
+            } 
+            if (voitto == true)
+            {
+                Console.Clear();
+                Console.WriteLine("Voitit pelin! Onneksi olkoon! " + "Sana oli " + arvattavaSana + ".");
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Hävisit pelin. Oikea vastaus oli: " + arvattavaSana);
+            }
         }
     }
 }
--- Hirsipuu-peli/Utils.cs
+++ Hirsipuu-peli/Utils.cs
@@ -1,27 +1,78 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace Hirsipuu_peli
 {
     class Utils
     {
-        public static List<string> Sanat() // eritelty metodina erikseen helpottaakseen sanojen lisäämistä
+        // eritelty metodina erikseen helpottaakseen sanojen lisäämistä
+        public static List<string> Sanat() 
         {
             List<string> sanalista = new List<string>();
             sanalista.Add("Gepardi");
             sanalista.Add("Iguaani");
             sanalista.Add("Kärsämäki");
+            sanalista.Add("Labyrintti");
+            sanalista.Add("Pyramidi");
+            sanalista.Add("Korsi");
+            sanalista.Add("Sotka");
+            sanalista.Add("Kaivo");
             return sanalista;
         }
 
-        public static string ArvotaanSana() // tällä arvotaan sana
+        // Tällä arvotaan arvattava sana sanalistasta
+        public static string ArvoSana() 
         {
             Random rnd = new Random();
             List<string> arvottavatSanat = Sanat();
             int rndIndeksi = rnd.Next(0, arvottavatSanat.Count);
             string arvottuSana = arvottavatSanat[rndIndeksi].ToUpper();
             return arvottuSana;
+        }
+
+        // Muuttaa arvotun sanan pelaajalle näytettäväksi salatuksi sanaksi
+        public static string MuutaSana(string arvottuSana) 
+        {
+            StringBuilder piilosanaBuilder = new StringBuilder(arvottuSana.Length);
+            for (int i = 0; i < arvottuSana.Length; i++)
+            {
+                piilosanaBuilder.Append("-");
+            }
+            string piilosana = piilosanaBuilder.ToString();
+            return piilosana;
+        }
+
+        // Paljastaa salatusta sanasta kirjaimia ja palauttaa päivitetyn salatun sanan
+        public static string PaljastaOsaSana(List<char>oikeatVastaukset, string arvottuSana) // 
+        {
+            StringBuilder näytäPelaajalle = new StringBuilder(arvottuSana.Length);
+            {
+                for (int i = 0; i < arvottuSana.Length; i++)
+                {
+                    if (oikeatVastaukset.Contains(arvottuSana[i]))
+                    {
+                        näytäPelaajalle.Append(arvottuSana[i]);
+                    }
+                    else
+                    {
+                        näytäPelaajalle.Append("-");
+                    }
+                }
+                return näytäPelaajalle.ToString();
+            }
+        }
+
+        // Tulostaa väärät sanat
+        public static string tulostaVäärät(List<char>väärätVastaukset)
+        {
+            StringBuilder väärätKirjaimet = new StringBuilder(väärätVastaukset.Count);
+            foreach (char value in väärätVastaukset)
+            {
+                väärätKirjaimet.Append(value + " ");
+            }
+            return väärätKirjaimet.ToString();
         }
     }
 }
